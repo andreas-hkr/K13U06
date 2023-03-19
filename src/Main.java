21ef798bd8646d151ae12fc919c7364145aa49b9
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 14:03:56 +0100

    Added english translation to comment

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // - Skapa en klass som kan representera ett kort från en kortlek
         // - Använd konstanter för att representera färg
-        //     KLÖVER = 0       RUTER = 1
-        //     HJÄRTER = 2      SPADER = 3
+        //     KLÖVER (CLUB) = 0            RUTER (DIAMOND) = 1
+        //     HJÄRTER (HEART)  = 2         SPADER (SPADES) = 3
         //
         // Skapa två objekt (Spader Dam och Klöver 7)
 
