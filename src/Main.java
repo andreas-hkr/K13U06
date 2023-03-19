bb0cb0cd67b9f966e45629f4d1b15e10651eaf6e
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 13:58:33 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // - Skapa en klass som kan representera ett kort från en kortlek
+        // - Använd konstanter för att representera färg
+        //     KLÖVER = 0       RUTER = 1
+        //     HJÄRTER = 2      SPADER = 3
+        //
+        // Skapa två objekt (Spader Dam och Klöver 7)
+
+
+    }
+}
